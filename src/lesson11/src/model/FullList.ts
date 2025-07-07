ce5b1ba11274c965e0942eac6a4ea63ada555f89
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-07 12:02:11 +0100

    Feat: Add ListTemplate interface and setup methods to 'CRUD' list elements for our list data

--- src/lesson11/src/model/FullList.ts
+++ src/lesson11/src/model/FullList.ts
@@ -11,7 +11,7 @@ interface List {
 
 export default class FullList implements List{
 
-    private instance: FullList = new FullList()
+    static instance: FullList = new FullList()
 
     private constructor(private _list: ListItem[] = []) {}
 
@@ -21,30 +21,37 @@ export default class FullList implements List{
 
     load(): void {
         const storedList: string | null = localStorage.getItem("myList")
+        //If storedList is not a string return.
         if (typeof storedList !== "string") return
 
-        const parsedList: {_id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)b
+        //storedList is parsed from JSON to an array of objects.
+        const parsedList: {_id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)
 
+        //For each object in parsedList create a new ListItem and use the static instance to add 'newListItem' to FullList.
         parsedList.forEach(itemObj => {
             const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked)
             FullList.instance.addItem(newListItem)
          })
     }
 
+    //Sets the list to JSON and saves it to local storage as "myList"
     save(): void{
         localStorage.setItem("myList", JSON.stringify(this._list))
     }
 
+    //Sets list as empty array and saves to local storage 
     clearList(): void {
         this._list = []
         this.save()
     }
 
+    //Pushes passed in argument to list array and then saves the updated list to local storage
     addItem(itemObj: ListItem): void {
         this._list.push(itemObj)
         this.save()
     }
 
+    //Removes passed in argument from the list array using filter and then saves the updated list to local storage
     removeItem(id: string): void {
         this._list = this._list.filter(item => item.id !== id)
         this.save()

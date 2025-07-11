fc7cc081edbb1f596ea76579e1eb33c574245795
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-11 11:00:19 +0100

    Finished lesson14 tutorial

--- src/lesson14/src/App.tsx
+++ src/lesson14/src/App.tsx
@@ -1,5 +1,5 @@
 
-import Counter from "./counter"
+import Counter from "./Counter"
 import './App.css'
 
 function App() {
--- src/lesson14/src/counter.tsx
+++ src/lesson14/src/counter.tsx
@@ -1,24 +1,67 @@
 
-import { ReactNode, useState } from 'react'
+import { ChangeEvent, ReactNode, useReducer} from 'react'
+
+// Using 'useReducer' as an alternative to 'useState'.
+
+//Initial state of count is 0
+const initState = { count: 0, text: ''}
+
+
+const enum REDUCER_ACTION_TYPE {
+    INCREMENT, 
+    DECREMENT,
+    NEW_INPUT,
+}
+
+type ReducerAction = {
+    type: REDUCER_ACTION_TYPE, 
+    payload?: string, 
+}
+
+// reducer function using the above enum and ReducerAction.
+const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
+    switch (action.type) {
+        //Switches return different function depending on enum option.
+        case REDUCER_ACTION_TYPE.INCREMENT: 
+            return {...state, count: state.count + 1}
+        case REDUCER_ACTION_TYPE.DECREMENT: 
+            return {...state, count: state.count - 1}
+            case REDUCER_ACTION_TYPE.NEW_INPUT: 
+            return {...state, text: action.payload ?? ''}
+        default: 
+            throw new Error()
+    }
+}
+
 
 type ChildrenType = {
     children: (num: number) => ReactNode
 }
 
+
 const Counter = ({ children }: ChildrenType) => {
-    const [count, setCount] = useState<number>(1)
+
+    //Import useReducer function above as well as the initial state.
+    const [state, dispatch] = useReducer(reducer, initState)
+
+    //onClick functions for '+' and '-' buttons. 
+    const increment = () => dispatch({type: REDUCER_ACTION_TYPE.INCREMENT})
+    const decrement = () => dispatch({type: REDUCER_ACTION_TYPE.DECREMENT})
+    const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => dispatch({type: REDUCER_ACTION_TYPE.NEW_INPUT, 
+        payload: e.target.value
+    })
 
 
-    const increment = () => setCount(prev => prev + 1)
-    const decrement = () => setCount(prev => prev - 1)
 
     return(
         <>
-            <h1>{children(count)}</h1>
+            <h1>{children(state.count)}</h1>
             <div>
                 <button onClick={increment}>+</button>
                 <button onClick={decrement}>-</button>
             </div>
+            <input type="text" onChange={handleTextInput}></input>
+            <h2>{state.text}</h2>
         </> 
     )
 } 

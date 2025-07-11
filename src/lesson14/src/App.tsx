8fbb3f53c65b1d893675ea79710ffb0d7116f779
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-11 10:36:51 +0100

    Setup components for lesson

--- src/lesson14/src/App.tsx
+++ src/lesson14/src/App.tsx
@@ -1,11 +1,13 @@
 
+import Counter from "./counter"
 import './App.css'
 
 function App() {
+  
 
   return (
     <>
-      
+      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
     </>
   )
 }
